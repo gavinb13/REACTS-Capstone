77cc012c47f7efe7b1714f6610e3747ca201c4a8
Ethan Kies <59584532+ethankies@users.noreply.github.com>
2024-04-07 15:20:55 -0400

    Crowd script
    edited crowd script to be pubilc names (so you can choose form unity which animations to play). Fixed bug if you do that

--- Assets/Crowd.cs
+++ Assets/Crowd.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class Crowd : MonoBehaviour {
 
-	private string[] names = {"idle","applause","applause2","celebration","celebration2","celebration3"};
+	public string[] names = {"idle","applause","applause2","celebration","celebration2","celebration3"};
 
 	// Use this for initialization
 	void Start () {
 		Animation[] AudienceMembers = gameObject.GetComponentsInChildren<Animation>();
 		foreach(Animation anim in AudienceMembers){
-			string thisAnimation = names[Random.Range(0,5)];
+			string thisAnimation = names[Random.Range(0,names.Length)];
 			
 			anim.wrapMode = WrapMode.Loop;
 			anim.GetComponent<Animation>().CrossFade(thisAnimation);
