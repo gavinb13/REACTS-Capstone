83c8a4e6f46625e2f8067f5214fc65928604680a
Ethan Kies <59584532+ethankies@users.noreply.github.com>
2024-04-21 20:56:27 -0400

    Added a 5-second delay to the blood particle system stoppage in the Bandage script to simulate realistic timing for bleeding control upon bandage application. This was achieved by introducing a coroutine, StopBleeding, which is triggered in OnTriggerEnter when the bandage contacts a wound. This change enhances the realism of the interaction between the bandage and the wound in the game environment.

--- Assets/HighStressAssets/Scripts/Bandage.cs
+++ Assets/HighStressAssets/Scripts/Bandage.cs
@@ -5,21 +5,46 @@ using UnityEngine;
 public class Bandage : MonoBehaviour
 {
 
-
+    private Coroutine stopBleedingCoroutine;
     private void OnTriggerEnter(Collider other)
     {
 
 
         if (other.gameObject.CompareTag("Wound")) // The wound tag is still on the parent empty GameObject
         {
-            Debug.Log("I hit something!" + other.name);
+            // Start the delay coroutine
+            stopBleedingCoroutine = StartCoroutine(StopBleeding(other));
+        }
+
+       /* if (other.gameObject.CompareTag("Wound")) // The wound tag is still on the parent empty GameObject
+        {
+       
+
+
             ParticleSystem bloodParticles = other.transform.Find("Particle System").GetComponent<ParticleSystem>();
             if (bloodParticles != null)
             {
                 bloodParticles.Stop(); // Stop the particle system when the bandage touches the wound
                                        // Additional feedback can be added here
             }
-        }
 
+
+        }*/
+
+    }
+
+    private IEnumerator StopBleeding(Collider wound)
+    {
+        // Wait for 5 seconds
+        yield return new WaitForSeconds(5);
+
+        // Access the ParticleSystem component
+        ParticleSystem bloodParticles = wound.transform.Find("Particle System").GetComponent<ParticleSystem>();
+
+        if (bloodParticles != null)
+        {
+            bloodParticles.Stop(); // Stop the particle system after 5 seconds
+            // Additional feedback can be added here such as sound effects or visual cues
+        }
     }
 }
