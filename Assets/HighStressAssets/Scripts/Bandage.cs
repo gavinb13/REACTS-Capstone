c2c1d1f5d3b8c8ed10ef166bc7db47c5070c8c3f
Ethan Kies <59584532+ethankies@users.noreply.github.com>
2024-04-22 00:50:32 -0400

    fixed STB EXCEPT FOR MEDIUM STRESS
    idk why but the particle system or box collider is not happy with me. gonna have to circle back to that

--- Assets/HighStressAssets/Scripts/Bandage.cs
+++ Assets/HighStressAssets/Scripts/Bandage.cs
@@ -9,11 +9,17 @@ public class Bandage : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
 
+        Debug.Log("Trigger entered. Colliding with: " + other.name);
 
         if (other.gameObject.CompareTag("Wound")) // The wound tag is still on the parent empty GameObject
         {
             // Start the delay coroutine
+            Debug.Log("Found the wound" + other.name);
+
+
             stopBleedingCoroutine = StartCoroutine(StopBleeding(other));
+
+            Debug.Log("Co routine done!");
         }
 
        /* if (other.gameObject.CompareTag("Wound")) // The wound tag is still on the parent empty GameObject
@@ -36,10 +42,12 @@ public class Bandage : MonoBehaviour
     private IEnumerator StopBleeding(Collider wound)
     {
         // Wait for 5 seconds
+
         yield return new WaitForSeconds(5);
 
         // Access the ParticleSystem component
-        ParticleSystem bloodParticles = wound.transform.Find("Particle System").GetComponent<ParticleSystem>();
+        //ParticleSystem bloodParticles = wound.transform.Find("Particle System").GetComponent<ParticleSystem>();
+        ParticleSystem bloodParticles = wound.GetComponentInChildren<ParticleSystem>(true);
 
         if (bloodParticles != null)
         {
