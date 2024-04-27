0243aa52de175e9d35a07bfa006b8a946d9e3924
gavinb13 <gavinbassett02@yahoo.com>
2024-04-27 12:59:11 -0400

    Back menu fix

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -10,34 +10,36 @@ public class LevelManager : MonoBehaviour
     public string sceneNameMediumStress;
     public string sceneNameHighStress;
     public string sceneNameHomeScreen;
-    public TMP_Text   hudText;
+    public TMP_Text hudText;
 
     public void ChangeSceneLow()
+    
     {
-        hudText.text = "Changing scene...";
+
+       
         SceneManager.LoadScene(sceneNameLowStress);
-        hudText.text = "";
+ 
 
     }
     public void ChangeSceneMedium()
     {
-        hudText.text = "Changing scene...";
+       
         SceneManager.LoadScene(sceneNameMediumStress);
-        hudText.text = "";
+       
 
     }
     public void ChangeSceneHigh()
     {
-        hudText.text = "Changing scene...";
+       
         SceneManager.LoadScene(sceneNameHighStress);
-        hudText.text = "";
+     
 
     }
     public void ChangeSceneHomeScreen()
     {
-        hudText.text = "Returning to main screen...";
+       
         SceneManager.LoadScene(sceneNameHomeScreen);
-        hudText.text = "";
+        
     } 
   
 }
